730d140e2ae576321aa3189ca0706c1e59eb5449
dbaton16 <98775246+dbaton16@users.noreply.github.com>
2023-09-15 10:27:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import * 
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
 import sys
-
+"""
+# Functions for calculations
+def calcAgeYears:
+    bframe.show()
+    currentDateTime = QDateTime.currentDateTime()
+    print((currentDateTime.tostring(Qt.ISODate)))
+"""
 # Function for the main window
 def window():
     # application
@@ -11,15 +17,21 @@ def window():
 
     # creating the window
     window = QMainWindow()
-    #window.setStyleSheet("background-color: grey;")
+    # window.setStyleSheet("background-color: grey;")
     # variables for window width and height
     Wwidth, Wheight = 500, 500
     window.setWindowTitle("Birthday GUI")
     window.setGeometry(500, 100, Wwidth, Wheight)
 
-    # divide into top and bottom subframe here
+    # bottom subframe here
+    bframe = QFrame(window)
+    bframe.setFrameShape(QFrame.StyledPanel)
+    bframe.setGeometry(0, 250, 500, 250)
+    bframe.setStyleSheet("background-color: #c0c7d1;")
+    bframe.hide() #hides frame until user enters info
+
     # maybe try to make frame hidden until user enters info
-    
+
     # creating the welcome to label
     welcome_label = QtWidgets.QLabel(window)
     welcome_label.setText("Welcome to a...")
@@ -34,29 +46,24 @@ def window():
     birthday_label.adjustSize()
     birthday_label.move(160, 30)
 
-
     # creating the entry box for entering birthday
     birthday_instruction = QtWidgets.QLabel(window)
     birthday_instruction.setText("Please enter your date of birth: ")
     birthday_instruction.setFont(QFont('Arial', 12))
     birthday_instruction.adjustSize()
     birthday_instruction.move(80, 100)
-    datetimeedit = QDateTimeEdit(window)
-    datetimeedit.setGeometry(100, 100, 150, 35)
-    datetimeedit.setDisplayFormat("yyyy/MM/dd")
-    #datetimeedit.setDisplayFormat("dd/MM/yyyyy")
-    #birthday_entry = QtWidgets.QTextEdit(window)
-    #birthday_entry.move(300, 100)
+    user_birthday = QDateTimeEdit(window)
+    user_birthday.setGeometry(300, 90, 150, 35)
+    user_birthday.setDisplayFormat("yyyy/MM/dd")
+    #user_birthday.clicked.connect
 
-    #handle keypress for user entry
-    def keyPressEvent(key):
-        if key.key() == Qt.Key_enter:
-            app.close()
 
-    #NEXT STEP: create a frame underneath, use connect method to connect widgets to functions and use alerts for zodiac
+
+    # NEXT STEP: use connect method to connect widgets to functions and use alerts for zodiac
 
     window.show()
     sys.exit(app.exec())
+
 
 window()
 
